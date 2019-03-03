802b67b323de3246dfd0b26fb6b8e8aca3b14ab6
guozishan <47099331+guozishan@users.noreply.github.com>
2019-03-03 23:08:26 +0800

    删除了一些没用的代码和文件

--- routes/web.php
+++ routes/web.php
@@ -18,40 +18,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home' , 'HomeController@index');
-//
-//Route::group(['prefix'=>'visiter' , 'middleware' => 'test'] , function(){
-//    Route::get('/blog' , 'BlogController@getAllBlog');//完成了
-//    Route::get('/blog/u/{user_id}' , 'BlogController@getBlogByAuthor');//完成了
-//    Route::get('/blog/{bid}' , 'BlogController@getBlogById');//完成了
-//});
 
-//Route::group(['middleware' => 'isLogin'] , function(){
-//    Route::get('/blog' , 'BlogController@getBlogByMySelf');//完成了
-//    Route::post('/blog' , 'BlogController@addBlog');
-//    Route::put('/blog/{bid}' , 'BlogController@updateBlog');
-//    Route::delete('/blog/{bid}' , 'BlogController@deleteBlog');
-//    Route::get('/blog/{bid}' , 'BlogController@getBlogById');//完成了
-//    Route::get('/comment/blog/{bid}' , 'commentController@getCommentByBlog');
-//});
-
-//Route::get('index', 'AddBlogController@index');//博文列表路由
-//Route::match(['get','post'],'/edit/{id}', 'AddPostController@edit')->where('id','[0-9]{1,3}');//同时处理留言编辑的界面展示和提交修改留言功能
-//Route::get('DeleteBlog', 'DeleteController@displayBlog');//删除博文
 Route::get('DeleteBlog/{mid}', 'BlogController@changeBlog');//删除博文
 Route::get('DeleteBlog/', function(){return view ('DeleteBlog');});
-
-//Route::get('comment', 'CommentController@Comment');//评论路由
 Route::get('/displayBlog/{mid}','BlogController@displayBlog');
 Route::get('/AllBlogs','BlogController@AllBlogs');
-//Route::get('GivenBlogs/', function(){return view ('GivenBlogs');});//展示特定某人博文
 Route::get('/GivenBlogs/{uname}','BlogController@GivenBlogs');
-//Route::get('AddBlog/', function(){return view ('AddBlog');});//博文添加界面展示
-Route::get('add', 'AddBlogController@add');
-
 Route::get('/wsl/{mid}', 'CommentController@index');
 Route::post('/AddComment/{mid}', 'CommentController@addComment');//博文评论添加界面展示
-Route::get('EditBlog{mid}/', function(){return view ('EditBlog');});//修改博文
-
 Route::get('/AddBlog_', 'BlogController@InputAddBlog');
 Route::post('/AddBlog', 'BlogController@addBlog');//博文添加
 Route::get('/EditBlog/{mid}', 'BlogController@InputEditBlog');
